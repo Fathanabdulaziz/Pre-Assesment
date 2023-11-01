64a90f46d441d0e70dd11fa4a7f1edb78792cdc1
Fathanabdulaziz <141543478+Fathanabdulaziz@users.noreply.github.com>
2023-11-01 20:17:04 +0700

    kedua.java

--- kedua.java
+++ kedua.java
@@ -1,50 +1,102 @@
 import java.util.Scanner;
 
-public class kedua {
+public class keduafix {
     static Scanner userInput = new Scanner(System.in);
 
-    public static void main(String[] args) {
-        System.out.println("Masukan harga pertama = ");
-        int pertama = userInput.nextInt();
-        System.out.println("Masukan harga kedua = ");
-        int kedua = userInput.nextInt();
-        
-        int hasilPertambahan = pertama + kedua;
-        
-        switch (hasilPertambahan) {
+    public static String konversiSatuan(int hasil) { //method satuan
+        switch (hasil) {
+            case 0:
+                return "nol";
             case 1:
-                System.out.print("satu");
-                break;
+                return "satu";
             case 2:
-                System.out.print("dua");
-                break;
+                return "dua";
             case 3:
-                System.out.print("tiga");
-                break;
+                return "tiga";
             case 4:
-                System.out.print("empat");
-                break;
+                return "empat";
             case 5:
-                System.out.print("lima");
-                break;
+                return "lima";
             case 6:
-                System.out.print("enam");
-                break;
+                return "enam";
             case 7:
-                System.out.print("tujuh");
-                break;
+                return "tujuh";
             case 8:
-                System.out.print("delapan");
-                break;
+                return "delapan";
             case 9:
-                System.out.print("sembilan");
-                break;
+                return "sembilan";
             default:
-                break;
-            }
-            String hasil = Integer.toString(hasilPertambahan).toLowerCase();
-            if ("hasil" = 1){
-                System.out.println("satu");
-            }
+                return "";
+        }
     }
+
+    public static String konversipuluhan(int hasil) { //method puluhan
+        switch (hasil) {
+            case 2:
+                return "dua puluh";
+            case 3:
+                return "tiga puluh";
+            case 4:
+                return "empat puluh";
+            case 5:
+                return "lima puluh";
+            case 6:
+                return "enam puluh";
+            case 7:
+                return "tujuh puluh";
+            case 8:
+                return "delapan puluh";
+            case 9:
+                return "sembilan puluh";
+            default:
+                return "";
+
+        }
+    }
+    public static void main(String[] args) {
+        
+
+        System.out.println("Masukan harga pertama = ");
+        int pertama = userInput.nextInt(); //user input angka ke 1
+        System.out.println("Masukan harga kedua = ");
+        int kedua = userInput.nextInt(); //user input angka ke 2
+
+        // rumus untuk mendeklarisikan 1 per 1:
+        int hasil = pertama + kedua;
+        int ratus = hasil / 100;
+        int ratusan = ratus % 10;
+        int puluh = hasil / 10;
+        int puluhan = puluh % 10;
+        int satuan = hasil % 10;
+
+        if (ratusan != 0) { //perkondisian tidak boleh 0 pada ratusan
+            if (ratusan == 1) {
+                System.out.print("Seratus ");
+            } else {
+                System.out.print(konversiSatuan(ratusan)+ " ratus ");
+            }
+        }
+
+        if (puluhan != 0) { // perkodndisian ketika puluhan
+            if (puluhan == 1) {
+                if (satuan == 0) {
+                    System.out.println("sepuluh");
+                    return;
+                } else if (satuan == 1) {
+                    System.out.print("sebelas");
+                    return;
+                }
+                System.out.print(konversiSatuan(satuan) + " belas ");
+                return;
+            } else {
+                System.out.print(konversipuluhan(puluhan) + " ");
+            }
+        }
+        if (satuan != 0) { //perkondisian ketika satuan 
+            System.out.print(konversiSatuan(satuan));
+        }
+        userInput.close();
+    }
+
 }
+
